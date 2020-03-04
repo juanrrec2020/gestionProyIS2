a3ed550665cde1142c78698d0508a6587c3b8001
jorgegauto <jorgegauto19@gmail.com>
2020-03-04 16:38:21 -0300

    Creacion del Proyecto

---
+++ inc/funciones/conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+$conn = new mysqli('localhost:8889', 'root', 'root', 'uptask');
+
+//Para comprobar la conexion a la base de datos
+/*
+echo "<pre>";
+var_dump($conn);
+echo "</pre>";
+*/
+
+//para verificar la conexion a la base de datos, si no retorana ningun mensaje en la cabecera entonces la conexion
+//fue exitosa, en caso contrario retorna un error de know de database
+if($conn->connect_error){
+    echo $conn->connect_error;
+}
+
+$conn ->set_charset('utf8');
