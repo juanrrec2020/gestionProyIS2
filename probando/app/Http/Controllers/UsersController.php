089ce870c9d5285a2f0a8a09c2b18aae4c35dc85
Ana Fretes <anafretes@fpuna.edu.py>
2020-03-15 23:00:32 -0300

    probando. voy a deshacer nomas si sale

---
+++ probando/app/Http/Controllers/UsersController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('usuarios.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        $roles = Role::all()->pluck('name', 'id');
+
+        return view('usuarios.create', compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $usuario = new User;
+
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = bcrypt($request->password);
+
+        if($usuario->save()){
+            //asignar el rol
+            $usuario->assignRole($request->rol);
+            return redirect('/usuarios');
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $usuario = User::findOrFail($id);
+        $roles = Role::all()->pluck('name', 'id');
+        return view('usuarios.edit', compact('usuario', 'roles'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $usuario = User::finOrFail($id);
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        if($request->password != null){
+            $usuario->password = $request-> password;
+        }
+        
+        $usuario->syncRoles($request->rol);
+        $usuario->save();
+        return redirect('/usuarios');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $usuario = User::findOrFail($id);
+        #Eliminar el role
+        $usuario->removeRole($usuario->roles->implode('name', ', '));
+        #Eliminar el usuario
+        if($usuario-delete())
+        {
+            return redirect('/usuarios');
+        }else {
+            response()->json([
+                'mensaje' => 'Error al eliminar el usuario'
+            ]);
+        }
+    }
+}
