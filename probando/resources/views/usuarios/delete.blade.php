af0e5fd65682b5004a8e40781139898967e29f7d
Ana Fretes <anafretes@fpuna.edu.py>
2020-03-15 23:03:39 -0300

    Revert "probando. voy a deshacer nomas si sale"
    This reverts commit 089ce870c9d5285a2f0a8a09c2b18aae4c35dc85.

--- probando/resources/views/usuarios/delete.blade.php
+++
@@ -1,8 +0,0 @@
-<form action="{{ route('usuarios.destroy', $usuario->id) }}" method="post" style="display:inline-block;">
-    @method('DELETE')
-    @csrf
-    <button type="submit" class="btn btn-danger">Eliminar</button>
-
-
-
-</form>
